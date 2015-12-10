0ddd677e7dae722de05373d7ced30a4849ab0576
Troels Ugilt Jensen <tujensen@gmail.com>
2015-12-10 12:42:27 +0100

    Issue #GGLASS-18 by tuj: Added video capture

--- app/src/main/java/itk/aakb/dk/gg_saarpleje/MainActivity.java
+++ app/src/main/java/itk/aakb/dk/gg_saarpleje/MainActivity.java
@@ -8,18 +8,23 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.glass.content.Intents;
 import com.google.android.glass.view.WindowUtils;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
     private static String TAG = "saarpleje";
+    private static final int TAKE_PICTURE_REQUEST = 1;
+    private static final int RECORD_VIDEO_CAPTURE_REQUEST = 2;
 
     private int imageIndex = 0;
     private String[] imagePaths = new String[2];
+    private List<String> videoPaths = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -29,6 +34,8 @@ public class MainActivity extends Activity {
         // window feature, be sure to request this before
         // setContentView() is called
         getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
+
+        // Set the main activity view.
         setContentView(R.layout.activity_layout);
     }
 
@@ -38,6 +45,7 @@ public class MainActivity extends Activity {
             getMenuInflater().inflate(R.menu.main, menu);
             return true;
         }
+
         // Pass through to super to setup touch menu.
         return super.onCreatePanelMenu(featureId, menu);
     }
@@ -69,7 +77,7 @@ public class MainActivity extends Activity {
                 case R.id.record_video_menu_item:
                     Log.i("saarpleje", "record video");
 
-                    // @TODO: Handle
+                    recordVideo();
 
                     break;
                 case R.id.finish_menu_item:
@@ -83,18 +91,27 @@ public class MainActivity extends Activity {
             }
             return true;
         }
-        // Good practice to pass through to super if not handled
+
+        // Pass through to super if not handled
         return super.onMenuItemSelected(featureId, item);
     }
 
-
-    private static final int TAKE_PICTURE_REQUEST = 1;
-
+    /**
+     * Launch a camera image capture intent.
+     */
     private void takePicture() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, TAKE_PICTURE_REQUEST);
     }
 
+    private void recordVideo() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, RECORD_VIDEO_CAPTURE_REQUEST);
+    }
+
+    /**
+     * When the camera activity returns, it is intercepted in this method.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
@@ -102,24 +119,59 @@ public class MainActivity extends Activity {
 
             processPictureWhenReady(picturePath);
         }
+        else if (requestCode == RECORD_VIDEO_CAPTURE_REQUEST && resultCode == RESULT_OK) {
+            String videoPath = data.getStringExtra(Intents.EXTRA_VIDEO_FILE_PATH);
+
+            processVideoWhenReady(videoPath);
+        }
 
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /**
+     * Update the UI when an image is set.
+     *
+     * @param index
+     */
+    private void imageIsSet(int index) {
+        Log.i(TAG, "Image is set");
+
+        ImageView imageView = null;
+
+        if (index == 0) {
+            imageView = (ImageView) findViewById(R.id.image_view_1);
+        }
+        else if (index == 1) {
+            imageView = (ImageView) findViewById(R.id.image_view_2);
+        }
+
+        imageView.setImageResource(R.drawable.ic_accept);
+        imageView.invalidate();
+    }
+
+    /**
+     * Process the picture.
+     *
+     * @param picturePath path to the image.
+     */
     private void processPictureWhenReady(final String picturePath) {
         final File pictureFile = new File(picturePath);
 
         if (pictureFile.exists()) {
-            // The picture is ready; process it.
+            // The picture is ready. We are not gonna work with it, but now we know it has been
+            // saved to disc.
 
             imagePaths[imageIndex] = picturePath;
 
             Log.i(TAG, "Picture " + imageIndex + " ready, with path: " + picturePath);
+
+            imageIsSet(imageIndex);
         } else {
             // The file does not exist yet. Before starting the file observer, you
             // can update your UI to let the user know that the application is
             // waiting for the picture (for example, by displaying the thumbnail
             // image and a progress indicator).
+            // @TODO: Add progress bar. Return to main menu when image is ready.
 
             final File parentDirectory = pictureFile.getParentFile();
             FileObserver observer = new FileObserver(parentDirectory.getPath(),
@@ -145,6 +197,63 @@ public class MainActivity extends Activity {
                                 @Override
                                 public void run() {
                                     processPictureWhenReady(picturePath);
+                                }
+                            });
+                        }
+                    }
+                }
+            };
+            observer.startWatching();
+        }
+    }
+
+
+    /**
+     * Process the video.
+     *
+     * @param videoPath path to the image.
+     */
+    private void processVideoWhenReady(final String videoPath) {
+        final File videoFile = new File(videoPath);
+
+        if (videoFile.exists()) {
+            // The picture is ready. We are not gonna work with it, but now we know it has been
+            // saved to disc.
+
+            videoPaths.add(videoPath);
+
+            Log.i(TAG, "Video ready, with path: " + videoPath);
+        } else {
+            // The file does not exist yet. Before starting the file observer, you
+            // can update your UI to let the user know that the application is
+            // waiting for the picture (for example, by displaying the thumbnail
+            // image and a progress indicator).
+            // @TODO: Add progress bar. Return to main menu when video is ready.
+
+            final File parentDirectory = videoFile.getParentFile();
+            FileObserver observer = new FileObserver(parentDirectory.getPath(),
+                    FileObserver.CLOSE_WRITE | FileObserver.MOVED_TO) {
+                // Protect against additional pending events after CLOSE_WRITE
+                // or MOVED_TO is handled.
+                private boolean isFileWritten;
+
+                @Override
+                public void onEvent(int event, String path) {
+                    if (!isFileWritten) {
+                        // For safety, make sure that the file that was created in
+                        // the directory is actually the one that we're expecting.
+                        File affectedFile = new File(parentDirectory, path);
+                        isFileWritten = affectedFile.equals(videoFile);
+
+                        if (isFileWritten) {
+                            stopWatching();
+
+                            // Now that the file is ready, recursively call
+                            // processPictureWhenReady again (on the UI thread).
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    processVideoWhenReady(videoPath);
                                 }
                             });
                         }
