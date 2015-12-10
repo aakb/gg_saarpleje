208c874a830aca30667e683d199da2af8381ca98
Troels Ugilt Jensen <tujensen@gmail.com>
2015-12-10 11:07:37 +0100

    Issue #GGLASS-18 by tuj: Added take picture intent.

--- app/src/main/java/itk/aakb/dk/gg_saarpleje/MainActivity.java
+++ app/src/main/java/itk/aakb/dk/gg_saarpleje/MainActivity.java
@@ -1,13 +1,25 @@
 package itk.aakb.dk.gg_saarpleje;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.FileObserver;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.glass.content.Intents;
 import com.google.android.glass.view.WindowUtils;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
+    private static String TAG = "saarpleje";
+
+    private int imageIndex = 0;
+    private String[] imagePaths = new String[2];
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -43,17 +55,19 @@ public class MainActivity extends Activity {
                 case R.id.take_before_image_menu_item:
                     Log.i("saarpleje", "take before image");
 
-                    // @TODO: Handle
+                    imageIndex = 0;
+                    takePicture();
 
                     break;
                 case R.id.take_after_image_menu_item:
                     Log.i("saarpleje", "take after image");
 
-                    // @TODO: Handle
+                    imageIndex = 1;
+                    takePicture();
 
                     break;
-                case R.id.take_video_menu_item:
-                    Log.i("saarpleje", "take video");
+                case R.id.record_video_menu_item:
+                    Log.i("saarpleje", "record video");
 
                     // @TODO: Handle
 
@@ -71,5 +85,73 @@ public class MainActivity extends Activity {
         }
         // Good practice to pass through to super if not handled
         return super.onMenuItemSelected(featureId, item);
+    }
+
+
+    private static final int TAKE_PICTURE_REQUEST = 1;
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TAKE_PICTURE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
+            String picturePath = data.getStringExtra(Intents.EXTRA_PICTURE_FILE_PATH);
+
+            processPictureWhenReady(picturePath);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void processPictureWhenReady(final String picturePath) {
+        final File pictureFile = new File(picturePath);
+
+        if (pictureFile.exists()) {
+            // The picture is ready; process it.
+
+            imagePaths[imageIndex] = picturePath;
+
+            Log.i(TAG, "Picture " + imageIndex + " ready, with path: " + picturePath);
+        } else {
+            // The file does not exist yet. Before starting the file observer, you
+            // can update your UI to let the user know that the application is
+            // waiting for the picture (for example, by displaying the thumbnail
+            // image and a progress indicator).
+
+            final File parentDirectory = pictureFile.getParentFile();
+            FileObserver observer = new FileObserver(parentDirectory.getPath(),
+                    FileObserver.CLOSE_WRITE | FileObserver.MOVED_TO) {
+                // Protect against additional pending events after CLOSE_WRITE
+                // or MOVED_TO is handled.
+                private boolean isFileWritten;
+
+                @Override
+                public void onEvent(int event, String path) {
+                    if (!isFileWritten) {
+                        // For safety, make sure that the file that was created in
+                        // the directory is actually the one that we're expecting.
+                        File affectedFile = new File(parentDirectory, path);
+                        isFileWritten = affectedFile.equals(pictureFile);
+
+                        if (isFileWritten) {
+                            stopWatching();
+
+                            // Now that the file is ready, recursively call
+                            // processPictureWhenReady again (on the UI thread).
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    processPictureWhenReady(picturePath);
+                                }
+                            });
+                        }
+                    }
+                }
+            };
+            observer.startWatching();
+        }
     }
 }
