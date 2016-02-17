459e166007b711732f27184b56a99871e5235fd1
Troels Ugilt Jensen <tujensen@gmail.com>
2016-02-17 14:16:44 +0100

    Issue #GGLASS-80 by tuj: Increased thressholds

--- app/src/main/java/itk/aakb/dk/gg_saarpleje/MemoActivity.java
+++ app/src/main/java/itk/aakb/dk/gg_saarpleje/MemoActivity.java
@@ -189,7 +189,7 @@ public class MemoActivity extends Activity {
                 SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
                 SensorManager.getOrientation(mR, mOrientation);
 
-                if (Math.abs(mOrientation[1]) < 0.20 && mOrientation[2] > 0) {
+                if (Math.abs(mOrientation[1]) < 0.20 && mOrientation[2] > 1) {
                     Log.i(TAG, "0: " + mOrientation[0] + "   1: " + mOrientation[1] + "   2: " + mOrientation[2]);
                     if (recording && timerExecutions > 0) {
                         Log.i(TAG, "Stop recording!");
